669cdd0c9e1485a66a7e4dddeec49ad765c2b2d8
정승현 <armyso9506@gmail.com>
2025-03-03 03:14:32 +0900

    [ Image ] Feature: 기본 이미지 컴포넌트화
    [ Image ] Feature: 기본 이미지 컴포넌트화

--- src/components/image-contents.tsx
+++ src/components/image-contents.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import { useEffect, useRef, useState } from "react";
-import Image from "next/image";
+import {useEffect, useRef, useState} from "react";
 
 type Props = {
   num: number;
@@ -22,9 +21,9 @@ const ImageContents = ({ num, onCloseCall }: Props) => {
       setVisible(true); // 마지막에는 이미지가 보이도록 설정
     }, 4000); // 7초 후 멈춤
 
-    setTimeout(() => {
-      onCloseCall();
-    }, 7000);
+    // setTimeout(() => {
+    //   onCloseCall();
+    // }, 7000);
 
     return () => clearInterval(interval);
   }, [onCloseCall]);
@@ -33,20 +32,27 @@ const ImageContents = ({ num, onCloseCall }: Props) => {
     <>
       <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-[rgba(0,0,0,0.80)]">
         <div className="relative w-full h-full">
-          <Image
-            src="/img/person.png"
-            alt="person"
-            width={1080}
-            height={1920}
-            className="w-full h-full object-cover transition-opacity duration-300"
-            style={{ opacity: visible ? 1 : 0 }}
-          />
-          <span
-            className="absolute inset-0 flex top-[25vh] justify-center text-white text-[400px] font-bold  transition-opacity duration-300"
-            style={{ opacity: visible ? 1 : 0 }}
-          >
-            {numRef.current}
-          </span>
+          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white w-[80%] h-[50%] rounded-[5em] px-[2%] pt-[2%]">
+            <div className="flex flex-col w-full h-full">
+              <div className="flex-1 w-full rounded-t-[5em] bg-blue-950 text-white flex justify-center items-center aspect-video">
+                <span
+                  className="text-white font-bold transition-opacity duration-300"
+                  style={{
+                    opacity: visible ? 1 : 0,
+                    fontSize: "clamp(50px, 35vw, 900px)", // 글자 크기 조정
+                  }}
+                >
+                  {numRef.current}
+                </span>
+              </div>
+
+              <div className="flex w-full h-[30%] justify-evenly items-center text-[20vw] font-[900] text-blue-950">
+                <span>고</span>
+                <span>객</span>
+                <span>님</span>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
