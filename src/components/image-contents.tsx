fce1c6ce949d469dcfc644f35178a24c67886075
정승현 <armyso9506@gmail.com>
2025-03-03 11:37:13 +0900

    [ Sound ] Fix: Sound 호출 Error 처리
    [ Sound ] Fix: Sound 호출 Error 처리

--- src/components/image-contents.tsx
+++ src/components/image-contents.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import {useEffect, useRef, useState} from "react";
+import { useEffect, useRef, useState } from "react";
 
 type Props = {
   num: number;
@@ -12,15 +12,18 @@ const ImageContents = ({ num, onCloseCall }: Props) => {
   const [visible, setVisible] = useState(true);
 
   useEffect(() => {
+    // 깜빡임 시작
     const interval = setInterval(() => {
       setVisible((prev) => !prev);
     }, 600); // 0.5초마다 깜빡이기
 
+    // 깜빡임 멈춤
     setTimeout(() => {
       clearInterval(interval);
       setVisible(true); // 마지막에는 이미지가 보이도록 설정
     }, 4000); // 7초 후 멈춤
 
+    // 호출 종류
     setTimeout(() => {
       onCloseCall();
     }, 7000);
@@ -32,9 +35,9 @@ const ImageContents = ({ num, onCloseCall }: Props) => {
     <>
       <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-[rgba(0,0,0,0.80)]">
         <div className="relative w-full h-full">
-          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white w-[80%] h-[50%] rounded-[5em] px-[2%] pt-[2%]">
+          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white w-[80%] h-[50%] rounded-[5em] px-[2%] pt-[1.6%]">
             <div className="flex flex-col w-full h-full">
-              <div className="flex-1 w-full rounded-t-[5em] bg-blue-950 text-white flex justify-center items-center aspect-video">
+              <div className="flex-1 w-full rounded-t-[4em] bg-blue-950 text-white flex justify-center items-center aspect-video">
                 <span
                   className="text-white font-bold transition-opacity duration-300"
                   style={{
