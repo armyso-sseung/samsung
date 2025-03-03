8941f15703a133f79d745ded054d4f2319a2abe3
정승현 <armyso9506@gmail.com>
2025-03-03 11:52:13 +0900

    [ Login ] Feature: 로그인 화면 개편
    [ Login ] Feature: 로그인 화면 개편

--- src/components/login-contents.tsx
+++ src/components/login-contents.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { MouseEvent, useCallback, useState } from "react";
+import Image from "next/image";
 
 type Props = {
   onLogin: () => void;
@@ -20,31 +21,37 @@ const LoginContents = ({ onLogin }: Props) => {
   );
 
   return (
-    <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black">
-      <div className="w-[350px] h-[200px] bg-gray-100 flex flex-col justify-center items-center gap-6 rounded-xl">
+    <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black px-10">
+      <div className="min-w-[365px] w-[400px] h-[300px] bg-gray-100 flex flex-col items-center gap-6 rounded-xl pt-10 px-10 relative">
+        {/* 로고 */}
         <div className="flex justify-center">
-          <span className="font-bold text-4xl text-black">SAMSUNG</span>
+          <Image src="/img/logo.png" alt="logo" width={200} height={100} />
         </div>
 
-        <form>
+        <form className="w-full">
           <div className="flex flex-col justify-center items-center gap-3">
-            <div className="flex justify-center items-center gap-4">
+            {/* 비밀번호 */}
+            <div className="flex justify-center items-center gap-4 w-full">
               <label htmlFor="password">비밀번호</label>
               <input
                 id="password"
-                className="h-8"
+                className="h-8 flex-1"
                 type="password"
                 value={inputPassword}
                 onChange={(e) => setInputPassword(e.target.value)}
               />
             </div>
-            <button
-              className="border-2 w-full bg-green-500 text-white py-1 rounded-md font-bold"
-              type="submit"
-              onClick={(event) => handleCheckPassword(event)}
-            >
-              로그인
-            </button>
+
+            {/* 로그인 버튼 */}
+            <div className="w-full absolute px-10 bottom-0 pb-8">
+              <button
+                className="border-2 w-full bg-green-500 text-white py-1 rounded-md font-bold"
+                type="submit"
+                onClick={(event) => handleCheckPassword(event)}
+              >
+                로그인
+              </button>
+            </div>
           </div>
         </form>
       </div>
